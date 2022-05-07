3c883f02c801f672809e0af6d96ade45cccac45b
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 18:32:14 -0500

    Ejemplo de tipos de datos en java

---
+++ JavaTutorial/JavaDataTypes.java
@@ -0,0 +1,16 @@
+public class JavaDataTypes {
+    public static void main(String[] args) {
+        //Estos son tipos de datos primitivos
+        byte myNum1 = 100;
+        short myNum2 = 5000;
+        int myNum3 = 100000;
+        long myNum4 = 15000000000L;
+        float myNum5 = 5.75f;
+        double myNum6 = 19.99d;
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        char myGrade = 'B';
+        String greeting = "Hola Mundo";
+        //Tambien existen otros datos que no son primitivios
+    }
+}
