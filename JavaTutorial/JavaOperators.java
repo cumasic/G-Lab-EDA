c428265da2c9da56dae72617846f8a3417b32716
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 18:41:04 -0500

    Ejemplo de operaciones en java

---
+++ JavaTutorial/JavaOperators.java
@@ -0,0 +1,10 @@
+public class JavaOperators {
+    public static void main(String[] args) {
+        //Existen tipos de operaciones en java estas son algunas
+        int a = 100 + 50;
+        int b = 100 - 50;
+        int c = 100 * b;//Se pueden utilizar variables con elementos de las operaciones
+        int d = a / b;
+        int e = c % a;
+    }
+}
