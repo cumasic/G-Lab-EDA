829087729f80659149cab96990fdc19cde22b650
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 18:12:47 -0500

    Ejemplo Java Syntax

---
+++ JavaTutorial/JavaSyntax.java
@@ -0,0 +1,5 @@
+public class JavaSyntax { //Cada archivo java tiene un nombre el cual debe coincidir con el nombre de la clase
+    public static void main(String[] args) {//El método main() es obligatorio en todos los programas java
+      System.out.println("Hello World");
+    }
+  }
