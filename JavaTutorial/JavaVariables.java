b373acdb9e683cd71a772ee44f862b7e240ea1c2
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 18:25:15 -0500

    Ejemplo de Variables en Java

---
+++ JavaTutorial/JavaVariables.java
@@ -0,0 +1,12 @@
+public class JavaVariables {
+    public static void main(String[] args) {
+        //Existen muchos tipos de variables
+        int myNumero1 = 5;
+        int myNumero2 = 5;//Se pueden declarar mas de una variable
+        float myFloatNumero = 5.99f;
+        char myLetra = 'D';
+        boolean myBooleano = true;
+        String myTexto = "Hola";
+        System.out.println(myTexto + "Estudiante");//Se puede combinar variables y texto en un println()
+    }
+}
