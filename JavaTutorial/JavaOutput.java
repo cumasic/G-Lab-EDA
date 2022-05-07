09da042ad335c5ef163c497383c2ace9ace29a27
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 18:16:46 -0500

    Ejemplo de Java Output

---
+++ JavaTutorial/JavaOutput.java
@@ -0,0 +1,6 @@
+public class JavaOutput {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");//Se puede usar el método println() para imprimir texto
+        System.out.println("Esto es asombroso");
+    }
+}
