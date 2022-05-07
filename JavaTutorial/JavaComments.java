3053e173d1c32372990c5ff783506bc356aaf83d
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 18:19:09 -0500

    Ejemplo de Comentarios en Java

---
+++ JavaTutorial/JavaComments.java
@@ -0,0 +1,9 @@
+public class JavaComments {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Esto es asombroso");
+    }
+    //Se pueden usar comentarios en java con "//"
+    /*Tambien comentarios multilinea
+    asi por ejemplo*/
+}
