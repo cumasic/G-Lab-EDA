8577b5978159fda46d5164ebb8885fc586607ce3
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 19:24:42 -0500

    Ejemplo de la clase Math en java

---
+++ JavaTutorial/JavaMath.java
@@ -0,0 +1,10 @@
+public class JavaMath {
+    public static void main(String[] args) {
+        //Algunos ejemplos de la clase Math
+        Math.max(5, 10);//Devuelve el mayor número
+        Math.min(8, 13);//Devuelve el menor número
+        Math.sqrt(64);//Devuelve la raiz cuadrada
+        Math.abs(-4.7);//Devuelve el valor positivo
+        Math.random();//Devuelve un numero random entre 0 y 1, se puede cambiar con algunas operaciones 
+    }
+}
