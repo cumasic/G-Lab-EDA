f75552509599e959104760b720a818468b899ad4
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-07 19:31:14 -0500

    Ejemplo de Booleanos en java

---
+++ JavaTutorial/JavaBooleans.java
@@ -0,0 +1,9 @@
+public class JavaBooleans {
+    public static void main(String[] args) {
+        //Algunas operaciones nos pueden dar booleanos
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y);//Esta nos daria true
+        System.out.println(x == y);//Y esta nos daria false
+    }
+}
